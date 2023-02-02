d3a816c7b1358bfd66ebef53a1eccbb4a0d45757
HMK <hasanmusa05g@hotmail.com>
2023-02-02 20:11:53 +0300

    student panel, login-register added. Universities are got.

--- Controllers/AffiliationController.cs
+++ Controllers/AffiliationController.cs
@@ -6,12 +6,24 @@ using System.Web.Mvc;
 using MVC_Library.DataLayer.EntityFramework;
 using MVC_Library.Managers;
 using MVC_Library.Models.Entity;
+using System.Web.Security;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text;
+using System.Web.UI.WebControls;
 
 namespace MVC_Library.Controllers
 {
     public class AffiliationController : Controller
     {
         MemberManager mm = new MemberManager(new EFMemberDal());
+        DBKUTUPHANE_Entities db_entities = new DBKUTUPHANE_Entities();
+
+        //Get list of universities with HtmlAgilityPack
+        HtmlDocument doc = new HtmlDocument();
+        HtmlWeb web = new HtmlWeb();
+        
+        
         // GET: Affiliation
         public ActionResult Index()
         {
@@ -20,19 +32,49 @@ namespace MVC_Library.Controllers
 
         [HttpGet]
         public ActionResult Login()
-        {
+        {          
             return View();
         }
 
         [HttpPost]
         public ActionResult Login(MEMBER m)
         {
-            return View();
+            var bilgiler = db_entities.MEMBER.FirstOrDefault(x=>x.USERNAME == m.USERNAME && x.PASSWORD == m.PASSWORD);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.USERNAME , false);
+                Session["username"] = bilgiler.USERNAME;
+               
+                return RedirectToAction("Index","Panel");
+            }
+            else 
+            {
+                ViewBag.msg = "Başarısız!";
+                return View();
+            }
         }
 
         [HttpGet]
         public ActionResult Register()
         {
+            //Turkish char issue is solved with Encoding UTF-8 format
+            web.OverrideEncoding = Encoding.UTF8;
+            doc = web.Load("https://www.unibilgi.net/turkiyedeki-universitelerin-listesi/");
+            List<string> list_of_uni = new List<string>();
+            int ct = 2;
+            while (true)
+            {
+                var xpath = string.Format("//*[@id='post-23027']/div[2]/table[1]/tbody/tr[{0}]/td[2]", ct);
+                var node = doc.DocumentNode.SelectSingleNode(xpath);
+
+                if (node != null)
+                {
+                    list_of_uni.Add(node.InnerText);
+                }
+                else { break; }
+                ct++;
+            }
+            ViewBag.listuni = list_of_uni;
 
             return View();
         }
@@ -46,6 +88,8 @@ namespace MVC_Library.Controllers
                 if (m.USERNAME == item.USERNAME)
                 {
                     TempData["error"] = "Bu kullanıcı adı kullanılmaktadır!";
+                    ViewBag.msg = "Üyelik oluşturulamadı!";
+
                 }
             }
             if (TempData["error"] != null)
@@ -55,6 +99,7 @@ namespace MVC_Library.Controllers
             else
             {
                 ViewBag.error = "";
+                ViewBag.msg = "Üyelik başarıyla oluşturuldu!";
                 mm.TInsert(m);
             }
             return View();
---
+++ Controllers/PanelController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Library.Models.Entity;
+
+namespace MVC_Library.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: Panel
+        DBKUTUPHANE_Entities db_entities = new DBKUTUPHANE_Entities();
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Index()
+        {
+            var username = (string)Session["username"];
+            var loggeduser = db_entities.MEMBER.FirstOrDefault(x=>x.USERNAME == username);
+            return View(loggeduser);
+        }
+        
+        [HttpPost]
+        public ActionResult UpdatePassword(MEMBER m)
+        {
+            var user = (string)Session["username"];
+            var updateduser = db_entities.MEMBER.FirstOrDefault(x => x.USERNAME == user);
+            updateduser.PASSWORD = m.PASSWORD;
+            db_entities.SaveChanges();
+            return View();
+        }
+
+    }
+}
